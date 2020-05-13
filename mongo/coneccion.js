2069ad8ce8f41ffe8189dd34e9947e9fbd10b81f
cersosimo7 <62675363+cersosimo7@users.noreply.github.com>
2020-05-13 01:03:36 -0300

    Add files via upload

--- mongo/coneccion.js
+++ mongo/coneccion.js
@@ -7,31 +7,73 @@ const client = new mongoclient(uri, {useNewUrlParser:true, useUnifiedTopology:tr
 client.connect((err, result) =>{
     if(!err){
         console.log(chalk.blue('Cliente conectado'));
+        // Trae la colección de inventores
         let collection = result.db("sample_betp2").collection("inventors");
+        
+        // No sé si había que modificar esto, de por sí find() trae los 20 primeros, tengo entendido.
         collection.find().limit(20).toArray((err, result) => {
             console.log(result);
-            // insertar un nuevo inventor
-            const nuevoInventor = {
-                first: "Pedro",
-                last: "Perez",
-                year: 1987
-            }
-
-            collection.insertOne(nuevoInventor, (error, result) => {
-                if(!error){
-                    console.log(chalk.yellow("Inventor insertado correctamente"));
-                    //nuevoInventor.year = 2000;
-                    // update de un campo
-                    collection.updateOne({last: "Perez"}, {$set: {year: 2000}} , (err, result) => {
-                        console.log(chalk.yellow("Inventor actualizado correctamente"));
-                        collection.deleteOne({last: "Perez"}, (err, result) => {
-                            console.log(chalk.yellow("Inventor eliminado correctamente"));
-                        });
-                    });
-                }
-            });
+            crud(collection);
         });
     } else {
         console.log(chalk.red(err));
     }
 });
+
+// Insertar
+function insertInventor(collection){
+    return new Promise((resolve)=>{
+        const nuevoInventor = {
+                    first: "Brian",
+                    last: "Cersosimo",
+                    year: 1995
+                }
+        resolve(collection.insertOne(nuevoInventor));
+    });
+}
+
+// Actualizar
+function updateInventor(collection){
+    return new Promise((resolve)=>{
+        resolve(collection.updateOne({last: "Cersosimo"}, {$set: {year: 2000}}))
+    });
+}
+
+// Eliminar
+function deleteInventor(collection){
+    return new Promise((resolve)=>{
+        resolve(collection.deleteOne({last: "Cersosimo"}))
+    });
+}
+
+/**
+ * 
+ * @param {*} collection Colección de inventores
+ */
+// Función asincrónica con métodos que retornan promesas o mensajes de error.
+async function crud(collection){
+
+    await insertInventor(collection)
+        .then( () => {console.log(chalk.yellow("Inventor insertado correctamente"))
+        })
+        .catch(error => {
+            console.log("Error al insertar", error)
+        });
+
+    await updateInventor(collection)
+        .then( () => {
+            console.log(chalk.yellow("Inventor actualizado correctamente"))
+        })
+        .catch(error => {
+            console.log("Error al actualizar", error)
+        });
+
+    await deleteInventor(collection)
+        .then( () => {
+            console.log(chalk.yellow("Inventor eliminado correctamente"))
+        })
+        .catch(error => {
+            console.log("Error al eliminar", error)
+        });
+
+}
